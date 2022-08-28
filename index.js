24cf6fa9afddf2cde2a1eb569495a82a6fa51a12
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-08-28 23:51:46 +0200

    my drum kit is working now

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 function handleClick() {
-  alert("I got clicked!")
+
+  let clickedButton = this.innerHTML;
+  if (clickedButton == "w") {
+    let audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+  }
+
+  if (clickedButton == "a") {
+    let audio = new Audio("sounds/tom-2.mp3");
+    audio.play();
+  }
+
+  if (clickedButton == "s") {
+    let audio = new Audio("sounds/tom-3.mp3");
+    audio.play();
+  }
+
+  if (clickedButton == "d") {
+    let audio = new Audio("sounds/tom-4.mp3");
+    audio.play();
+  }
+
+  if (clickedButton == "j") {
+    let audio = new Audio("sounds/snare.mp3");
+    audio.play();
+  }
+
+  if (clickedButton == "k") {
+    let audio = new Audio("sounds/crash.mp3");
+    audio.play();
+  }
+  if (clickedButton == "l") {
+    let audio = new Audio("sounds/kick-bass.mp3");
+    audio.play();
+  }
 }
+
+
 
 let drums = document.querySelectorAll(".drum");
 
