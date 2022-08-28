6b215b49975c8aa1cd68e706818da8dce00f45dc
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-08-28 22:17:25 +0200

    activated all drum buttons

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-alert("Hello");
+function handleClick() {
+  alert("I got clicked!")
+}
+
+let drums = document.querySelectorAll(".drum");
+
+for (var i = 0; i < drums.length; i++) {
+  drums[i].addEventListener("click", handleClick);
+}
