614cdf1ca534febe2016c4f905278ee9584ef60e
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-08-30 21:20:51 +0200

    now key w playing sound

--- index.js
+++ index.js
@@ -1,44 +1,60 @@
+
+document.addEventListener("keydown", function keyDown(event) {
+  let pressedKey = event.key;
+  console.log(pressedKey);
+
+
+  if (pressedKey == "w" ) {
+    let audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+    console.log(pressedKey);
+  }
+});
+
+
 function handleClick() {
 
   let clickedButton = this.innerHTML;
   this.style.color = "white";
 
-  // if (clickedButton == "w") {
-  //   let audio = new Audio("sounds/tom-1.mp3");
-  //   audio.play();
-  // }
-  //
-  // if (clickedButton == "a") {
-  //   let audio = new Audio("sounds/tom-2.mp3");
-  //   audio.play();
-  // }
-  //
-  // if (clickedButton == "s") {
-  //   let audio = new Audio("sounds/tom-3.mp3");
-  //   audio.play();
-  // }
-  //
-  // if (clickedButton == "d") {
-  //   let audio = new Audio("sounds/tom-4.mp3");
-  //   audio.play();
-  // }
-  //
-  // if (clickedButton == "j") {
-  //   let audio = new Audio("sounds/snare.mp3");
-  //   audio.play();
-  // }
-  //
-  // if (clickedButton == "k") {
-  //   let audio = new Audio("sounds/crash.mp3");
-  //   audio.play();
-  // }
-  // if (clickedButton == "l") {
-  //   let audio = new Audio("sounds/kick-bass.mp3");
-  //   audio.play();
-  // }
+
+  if (clickedButton == "w" ) {
+    let audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+    console.log(pressedKey);
+  }
+
+  if (clickedButton == "a") {
+    let audio = new Audio("sounds/tom-2.mp3");
+    audio.play();
+  }
+
+  if (clickedButton == "s") {
+    let audio = new Audio("sounds/tom-3.mp3");
+    audio.play();
+  }
+
+  if (clickedButton == "d") {
+    let audio = new Audio("sounds/tom-4.mp3");
+    audio.play();
+  }
+
+  if (clickedButton == "j") {
+    let audio = new Audio("sounds/snare.mp3");
+    audio.play();
+  }
+
+  if (clickedButton == "k") {
+    let audio = new Audio("sounds/crash.mp3");
+    audio.play();
+  }
+  if (clickedButton == "l") {
+    let audio = new Audio("sounds/kick-bass.mp3");
+    audio.play();
+  }
 
 
-//   Created constructor function for playing audio
+  // Created constructor function for playing audio
 //   function Audio (fileLocation) {
 //     this.fileLocation = fileLocation;
 //     this.play = function();
@@ -51,45 +67,45 @@ function handleClick() {
 
 
 
-  switch (clickedButton) {
-    case "w":
-      let tom1 = new Audio("sounds/tom-1.mp3");
-      tom1.play();
-      break;
-
-    case "a":
-      let tom2 = new Audio("sounds/tom-2.mp3");
-      tom2.play();
-      break;
-
-    case "s":
-      let tom3 = new Audio("sounds/tom-3.mp3");
-      tom3.play();
-      break;
-
-    case "d":
-      let tom4 = new Audio("sounds/tom-4.mp3");
-      tom4.play();
-      break;
-
-    case "j":
-      let snare = new Audio("sounds/snare.mp3");
-      snare.play();
-      break;
-
-    case "k":
-      let crash = new Audio("sounds/crash.mp3");
-      crash.play();
-      break;
-
-    case "l":
-      let kickBass = new Audio("sounds/kick-bass.mp3");
-      kickBass.play();
-      break;
-
-    default: console.log(clickedButton);
-
-  }
+  // switch (clickedButton) {
+  //   case "w":
+  //     let tom1 = new Audio("sounds/tom-1.mp3");
+  //     tom1.play();
+  //     break;
+  //
+  //   case "a":
+  //     let tom2 = new Audio("sounds/tom-2.mp3");
+  //     tom2.play();
+  //     break;
+  //
+  //   case "s":
+  //     let tom3 = new Audio("sounds/tom-3.mp3");
+  //     tom3.play();
+  //     break;
+  //
+  //   case "d":
+  //     let tom4 = new Audio("sounds/tom-4.mp3");
+  //     tom4.play();
+  //     break;
+  //
+  //   case "j":
+  //     let snare = new Audio("sounds/snare.mp3");
+  //     snare.play();
+  //     break;
+  //
+  //   case "k":
+  //     let crash = new Audio("sounds/crash.mp3");
+  //     crash.play();
+  //     break;
+  //
+  //   case "l":
+  //     let kickBass = new Audio("sounds/kick-bass.mp3");
+  //     kickBass.play();
+  //     break;
+  //
+  //   default: console.log(clickedButton);
+  //
+  // }
 }
 
 function sleep(ms) {
