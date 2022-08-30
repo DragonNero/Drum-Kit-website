f35dc79b24defa9d010b07ea9890282eca0c39ef
Dragon Nero <76917759+DragonNero@users.noreply.github.com>
2022-08-30 22:23:36 +0200

    buttons and keys do sond

--- index.js
+++ index.js
@@ -1,57 +1,54 @@
-
-document.addEventListener("keydown", function keyDown(event) {
+document.addEventListener("keydown", function (event) {
   let pressedKey = event.key;
   console.log(pressedKey);
-
-
-  if (pressedKey == "w" ) {
-    let audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
-    console.log(pressedKey);
+  if (pressedKey === "w" || pressedKey === "a") {
+      handleSounds(pressedKey);
   }
+
 });
 
+function handleSounds(clickedButton) {
+    if (clickedButton == "w") {
+      let audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+    }
+
+    if (clickedButton == "a") {
+      let audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+    }
+
+    if (clickedButton == "s") {
+      let audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+    }
+
+    if (clickedButton == "d") {
+      let audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+    }
+
+    if (clickedButton == "j") {
+      let audio = new Audio("sounds/snare.mp3");
+      audio.play();
+    }
+
+    if (clickedButton == "k") {
+      let audio = new Audio("sounds/crash.mp3");
+      audio.play();
+    }
+    if (clickedButton == "l") {
+      let audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+    }
+}
 
 function handleClick() {
 
   let clickedButton = this.innerHTML;
   this.style.color = "white";
 
-
-  if (clickedButton == "w" ) {
-    let audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
-    console.log(pressedKey);
-  }
-
-  if (clickedButton == "a") {
-    let audio = new Audio("sounds/tom-2.mp3");
-    audio.play();
-  }
-
-  if (clickedButton == "s") {
-    let audio = new Audio("sounds/tom-3.mp3");
-    audio.play();
-  }
-
-  if (clickedButton == "d") {
-    let audio = new Audio("sounds/tom-4.mp3");
-    audio.play();
-  }
-
-  if (clickedButton == "j") {
-    let audio = new Audio("sounds/snare.mp3");
-    audio.play();
-  }
-
-  if (clickedButton == "k") {
-    let audio = new Audio("sounds/crash.mp3");
-    audio.play();
-  }
-  if (clickedButton == "l") {
-    let audio = new Audio("sounds/kick-bass.mp3");
-    audio.play();
-  }
+  handleSounds(clickedButton);
 
 
   // Created constructor function for playing audio
